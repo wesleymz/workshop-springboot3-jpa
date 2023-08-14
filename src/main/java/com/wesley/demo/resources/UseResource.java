140285c25a85db7bb7a5f77644c5bd7dff523fd8
Wesley Santos Silva (SYSMAP) <wesleysilva.sysmap@latam.com>
2023-08-14 10:10:01 -0300

    User entity and resource

---
+++ src/main/java/com/wesley/demo/resources/UseResource.java
@@ -0,0 +1,18 @@
+package com.wesley.demo.resources;
+
+import com.wesley.demo.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UseResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com.", "99999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
